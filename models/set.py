6bd968c92a78d7be3edea5953a586c8c68ccc499
CaptainOmNoms <gilg24@gmail.com>
2019-07-23 22:17:06 -0500

    Update set.py

--- models/set.py
+++ models/set.py
@@ -2,7 +2,7 @@ from db import db
 
 class SetModel(db.Model):
     __tablename__ = 'sets'
-    __table_args__ = db.UniqueConstraint('set_name', name='unique_name_commit')
+    __table_args__ = (db.UniqueConstraint('set_name', name='unique_name_commit'),)
 
     set_id = db.Column(db.Integer, primary_key=True)
     set_name = db.Column(db.String(32), primary_key=True)
