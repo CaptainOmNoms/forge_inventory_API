0a547138faf795b22e0b5dedf68960538d6e6045
CaptainOmNoms <gilg24@gmail.com>
2019-07-23 21:38:18 -0500

    Getting it to run on Heroku
    Adding PROPAGATE_EXCEPTIONS=True
    Setting TRACK_MODS=True
    changing module to app:app from run:app

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 api = Api(app)
 
